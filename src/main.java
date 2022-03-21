97d1fca269192cb9c3cd5f901f628d3678f93165
Byektilyeu <bekbektileu5@gmail.com>
2022-03-21 19:34:24 +0800

    class-uudad huvaaj, 3secund tutamd request ilgeedeg bolgoson

---
+++ src/main.java
@@ -0,0 +1,62 @@
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.XML;
+
+public class main {
+	
+	public static void main(String[] args) throws Exception {
+		
+		
+		new Timer().scheduleAtFixedRate(new TimerTask(){
+		    @Override
+		    public void run(){
+		    	try {
+		    		mainFunction();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    }
+		},0,3000);
+	}
+
+	private static void mainFunction() throws Exception{
+			try {
+				sslDisable ssl = new sslDisable();
+				ssl.disableSslVerification();
+				request instance = new request();
+				String response = instance.postMessage();
+//				String response = run();
+				String qmsNum = null;
+				JSONObject jsonObj = XML.toJSONObject(response);
+				JSONObject RK7QueryResult = (JSONObject) jsonObj.get("RK7QueryResult");
+				JSONObject CommandResult = (JSONObject) RK7QueryResult.get("CommandResult");
+				JSONObject Order = (JSONObject) CommandResult.get("Order");
+				JSONObject ExternalProps = (JSONObject) Order.get("ExternalProps");
+				
+				JSONArray arrProp = (JSONArray) ExternalProps.getJSONArray("Prop");
+				for(int i = 0; i < arrProp.length(); i ++) {
+					JSONObject Prop = (JSONObject) arrProp.getJSONObject(i);
+					String qmsNumber = (String) Prop.get("value");  
+					if(qmsNumber.length() == 3) {
+						qmsNum = qmsNumber;
+					}
+				}
+				
+				String kdsstate = (String) Order.get("kdsstate");  
+	            int visit = (int) Order.get("visit");         
+
+	            System.out.println("GETORDER XML DATA: " + response);
+	            System.out.println("GETORDER JSON DATA: " + jsonObj);
+	            System.out.println("visit: " + visit);
+	            System.out.println("kdsstate: " + kdsstate);
+				System.out.println("qmsNumber: " + qmsNum);
+			} catch(IOException ex) {
+				System.out.println("=======================" + ex);
+			}
+		}
+}
--- src/request.java
+++ src/request.java
@@ -1,41 +1,53 @@
-
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
- 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class request {
- 
 	public String postMessage() throws IOException {
 		final String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) Version/7.0.3 Safari/7046A194A";
 		final StringBuffer content = new StringBuffer();
-//		
-//		final String parameters = "?name=tecbar&group=middleschool";
-//		final URL url = new URL("http://tecbar.net/demo/param" + parameters);
+		final URL url = new URL("https://10.0.0.111:8086/rk7api/v0/xmlinterface.xml");
 		
-		final URL url = new URL("https://10.0.0.11:3640/rk7api/v0/xmlinterface.xml");
+		String username = "http_user1";
+		String password = "9";
+		String auth = username + ":" + password;
+		byte[] bytes = auth.getBytes(StandardCharsets.UTF_8); 
+		String base64Encoded = Base64.getEncoder().encodeToString(bytes);
+		String authHeaderValue = "Basic " + new String(base64Encoded);
+		System.out.println("Base64 encoded text: " + base64Encoded);
 		
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
+		HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+		
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Authorization", authHeaderValue);
 		conn.setRequestProperty("User-Agent", USER_AGENT);
-		conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8"); 
+		conn.setRequestProperty("Content-Type", "application/xml; charset=UTF-8"); 
+
 		conn.setDoOutput(true);
-		conn.setConnectTimeout(5000);
-		
-//		username: http_user1
-//		pass: 9
-// 
+		conn.setConnectTimeout(500);
+	
 		// Send post request
 		conn.setDoOutput(true);
-//		DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
-//		final String msg = "<channel>ABC</channel><title>tecbar.net demo pages</title>";
-//		wr.writeBytes(msg);
-		// send request
-//		wr.flush();
-//		// close
-//		wr.close();
+
+		System.out.println(" -------------------------");
+		DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+
+		final String msg = "<RK7Query>\r\n"
+				+ " <RK7Command CMD=\"GetOrder\">\r\n"
+				+ "  <Order guid=\"{9EA66BB1-502C-49E6-8876-DDE9299C7662}\"/>\r\n"
+				+ " </RK7Command>\r\n"
+				+ "</RK7Query>";
+		wr.writeBytes(msg);
+//		 send request
+		wr.flush();
+		// close
+		wr.close();
  
 		// read response
 		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
@@ -47,14 +59,5 @@ public class request {
  
 		return content.toString();
 	}
- 
-	public static void main(String[] args) {
-		try {
-			request instance = new request();
-			String response = instance.postMessage();
-			System.out.println(response);
-		} catch(IOException ex) {
-			// process the exception
-		}
-	}
+
 }
