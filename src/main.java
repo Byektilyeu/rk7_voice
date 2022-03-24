e0b70681adebd6a9baab4401cb4bd63f8953bbcc
Byektilyeu <bekbektileu5@gmail.com>
2022-03-24 13:16:56 +0800

    aaa

--- src/main.java
+++ src/main.java
@@ -99,6 +99,7 @@ public class main {
 				} else {
 					kdsstate = null;
 					System.out.println("!!! Уучлаарай, " + qmsNum + " захиалгын өгөгдөлд kdsstate олдоогүй !!!");
+					System.out.println("!!! Уучлаарай, " + qmsNum + " захиалгын өгөгдөлд kdsstate олдоогүй !!!");
 				}
 				
 				InsertApp app = new InsertApp();
